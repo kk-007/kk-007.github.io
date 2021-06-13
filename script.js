d50747440a78d85339df40a0b5378ee8980db312
Keval Navadiya <kevalnavadiya39@gmail.com>
2021-06-13 20:36:21 +0530

    updated

--- script.js
+++ script.js
@@ -5,11 +5,41 @@ var themeStyle = document.getElementById("theme-style");
 const darkThemeFile = "default.css";
 const lightThemeFile = "light.css";
 const darkImgUrl =
-  "https://github-readme-stats.vercel.app/api?username=kk-007&&show_icons=true&title_color=eeeeee&icon_color=fff&text_color=eeeeee&bg_color=164d56";
+    "https://github-readme-stats.vercel.app/api?username=kk-007&&show_icons=true&title_color=eeeeee&icon_color=fff&text_color=eeeeee&bg_color=164d56";
 const lightImgUrl =
-  "https://github-readme-stats.vercel.app/api?username=kk-007&&show_icons=true&title_color=000000&icon_color=000000&text_color=000000&bg_color=eaeaea";
+    "https://github-readme-stats.vercel.app/api?username=kk-007&&show_icons=true&title_color=000000&icon_color=000000&text_color=000000&bg_color=eaeaea";
 
 const changeTheme = () => {
-  themeStyle.href = toggle.checked ? darkThemeFile : lightThemeFile;
-  socialImg.src = toggle.checked ? darkImgUrl : lightImgUrl;
+    themeStyle.href = toggle.checked ? darkThemeFile : lightThemeFile;
+    socialImg.src = toggle.checked ? darkImgUrl : lightImgUrl;
 };
+
+const contactForm = document.forms["contact-form"];
+contactForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const payload = {
+        from: contactForm.from.value,
+        title: contactForm.title.value,
+        email: contactForm.email.value,
+        body: contactForm.body.value,
+    };
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify(payload);
+
+    var requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow",
+    };
+
+    fetch(
+        "https://contact-us-msg-api.herokuapp.com/api/message",
+        requestOptions
+    )
+        .then((response) => response.text())
+        .then((result) => alert("Message sent successfully"))
+        .catch((error) => console.log("error", error));
+});
